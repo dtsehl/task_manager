956303a3a1cddab436d0ee956dad62f159bb374f
Dan Sehl <dan.sehl@gmail.com>
2020-06-22 10:45:11 -0600

    Add tasks#show and tasks#edit to routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
@@ -5,4 +7,6 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
+  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id/edit', to: 'tasks#edit'
 end
