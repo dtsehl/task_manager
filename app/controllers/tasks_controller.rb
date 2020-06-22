d146eb145e53f5c188c9fa08aa7fef58614bbd45
Dan Sehl <dan.sehl@gmail.com>
2020-06-22 09:04:04 -0600

    Change #index method in tasks_controller to list generated tasks instead of static strings

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@
 
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
@@ -13,9 +13,7 @@ class TasksController < ApplicationController
       title: params[:task][:title],
       description: params[:task][:description]
       })
-
     task.save
-
     redirect_to '/tasks'
   end
 end
