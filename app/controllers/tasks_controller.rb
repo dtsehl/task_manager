2177c7689d247cd56da8aa9afaae2863e1a2a292
Dan Sehl <dan.sehl@gmail.com>
2020-06-22 10:42:04 -0600

    Add #edit method to tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,4 +16,12 @@ class TasksController < ApplicationController
     task.save
     redirect_to '/tasks'
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
 end
